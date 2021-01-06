00fa72f013955c487745106a35aef016f333ddea
Tanay Karnik <tanayvk@gmail.com>
2021-01-07 03:04:38 +0530

    Fix timezone issue

--- watch/views.py
+++ watch/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from .models import CoinRate
 import json
+import datetime
 
 def index(request):
     latest_rates = CoinRate.objects.order_by('-time')[:60]
-    times = list(map(lambda x: x.time.strftime("%H:%M:%S"), latest_rates))
+    times = list(map(lambda x: convertTime(x.time), latest_rates))
     btc = list(map(lambda x: float(x.btc), latest_rates))
     eth = list(map(lambda x: float(x.eth), latest_rates))
 
@@ -18,3 +19,6 @@ def index(request):
         "eth": json.dumps(eth)
     }
     return render(request, 'watch/index.html', context)
+
+def convertTime(time):
+    return time.strftime("%c %z")
